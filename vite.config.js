69d29cc776dde568a54cd302b5e686e934a3ea25
jigarswami1 <89700617+jigarswami1@users.noreply.github.com>
2026-01-07 17:20:01 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // IMPORTANT: set this to your repository name
+  // Example if your repo is latest-fishbone-builder:
+  base: '/latest-fishbone-builder/'
+})
